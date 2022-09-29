44d5c467df96b7a4ef977503cd2b3a4274d9ce11
Al Farabi Haikal El Fadhil <110651458+alfarabihe@users.noreply.github.com>
2022-09-29 20:33:21 +0700

    Add files via upload

---
+++ input_output.py
@@ -0,0 +1,33 @@
+# latihan input dan output
+
+# 01
+print("Hello World!")
+
+# 02
+print("Heloo", "World", sep="-")
+
+# 03
+print("Hello", end=" ")
+print("World!")
+
+# 04 # dengan koma
+print("Hello", "World")
+
+# 05 # dengan plus
+print("Hello" + "World!")
+
+# 06
+# ini merupakan komentar
+
+# 07
+"""
+ini juga komentar
+"""
+
+# 08
+x = input("Masukkan kalimat: ")
+print(f"kalimat anda adalah: {x}")
+
+# 09
+y = int(input("Masukkan angka: "))
+print(f"Jika angka anda dikali 2, maka hasilnya adalah: {y * 2}")
