1b878311647fec026c51a592f0c05be05de42aeb
Al Farabi Haikal El Fadhil <110651458+alfarabihe@users.noreply.github.com>
2022-09-29 23:31:26 +0700

    Add files via upload

---
+++ 05_latihan01.py
@@ -0,0 +1,8 @@
+# latihan genap-ganjil
+
+x = int(input("masukkan bilangan: "))
+
+if x % 2 == 0:
+    print(f"bilangan {x} merupakan bilangan genap")
+else:
+    print(f"bilangan {x} merupakan bilangan ganjil")
