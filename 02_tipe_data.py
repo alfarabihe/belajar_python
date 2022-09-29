64e5750cea4afe6ec3b3142a82dd5f61cd53792b
Al Farabi Haikal El Fadhil <110651458+alfarabihe@users.noreply.github.com>
2022-09-29 20:50:57 +0700

    Add files via upload

---
+++ 02_tipe_data.py
@@ -0,0 +1,27 @@
+# latihan tipe data
+
+# boolean
+a = True
+b = False
+print(a, b)
+
+# integer
+c = 5
+d = 19
+print(c, d)
+
+# float
+e = 10.3
+f = 76.317
+print(e, f)
+
+# string
+g = "belajar python"
+h = 'belajar python'
+print(g, h)
+
+# mengecek
+print(f"{type(a)} {type(b)}\n"
+    f"{type(c)} {type(d)}\n"
+    f"{type(e)} {type(f)}\n"
+    f"{type(g)} {type(h)}")
