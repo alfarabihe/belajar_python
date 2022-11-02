70a105a2e3352ae99daec9d463f49fbebbf2c8a3
Al Farabi Haikal El Fadhil <110651458+alfarabihe@users.noreply.github.com>
2022-11-02 18:04:44 +0700

    Add files via upload

---
+++ [02]Menjumlahkan.py
@@ -0,0 +1,13 @@
+# Program Test
+# Menjumlahkan nilai A dan B
+
+# KAMUS
+# a, b, jumlah : int
+
+# ALGORITMA
+a = int(input("Masukkan nilai A: ")) # input
+b = int(input("Masukkan nilai B: ")) # input
+
+jumlah = a + b                       # proses
+
+print(f"{a} + {b} = {jumlah}")       # output
